a73da43df9459512a8ebac05eb2f6f8b52827cb0
Givi <gvtsvariani@gmail.com>
2026-06-18 18:41:11 -0400

    markdown module for frontend, might not be the way to go, but we'll see, i dont like that it can aslo return a promise so ill cast to string and hope for the best its just a string of md what could go wrong (watch it all crumble)

---
+++ apps/web/src/lib/markdown.ts
@@ -0,0 +1,5 @@
+import * as marked from "marked";
+
+export function renderMarkdown(md: string): string {
+    return marked.marked.parse(md) as string; // i might want to read the docs for this in detail huh
+}
