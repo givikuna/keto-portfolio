79869c618f8ebd8ac4da5cf7b5a5e780b6e1678f
Givi <gvtsvariani@gmail.com>
2026-06-22 20:26:19 -0400

    contact page drawn

--- apps/server/routes/api/routes.ts
+++ apps/server/routes/api/routes.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
-import * as nodemailer from "nodemailer";
+import * as child_process from "child_process";
+import * as util from "util";
 
 import { DB_API } from "../../../db/api";
 
@@ -52,14 +53,36 @@ router.get("/cmd", (_req: express.Request, _res: express.Response): void => {
 });
 
 router.post("/contact", async (req: express.Request, res: express.Response): Promise<void> => {
-    if (!req.body["name"] || !req.body["email"] || !req.body["message"]) {
-        res.status(400).json({ error: "name, email, and message are required" });
-        return;
-    }
+    try {
+        if (!req.body["name"] || !req.body["email"] || !req.body["message"]) {
+            res.status(400).json({ error: "name, email, and message are required" });
+            return;
+        }
 
-    const transporter = nodemailer.createTransport({
-        //
-    });
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(req.body["email"])) {
+            res.status(400).json({ error: "invalid email address" });
+            return;
+        }
+        const subject: string = `Portfolio contact from ${req.body["name"]}`.replace(/"/g, '\\"');
+        const body: string = `
+New Message:
+
+Name: ${req.body["name"]}
+Email: ${req.body["email"]}
+Message:
+${req.body["message"].trim()}
+    `
+            .replace(/"/g, '\\"')
+            .trim();
+
+        await util.promisify(child_process.exec)(
+            `echo "${body}" | mail -s "${subject}" ${DB_API.getPersonals().email}`,
+            { shell: "/bin/sh" },
+        );
+    } catch (e: unknown) {
+        console.error("error sending mail:", e);
+        res.status(500).json({ error: "failed to send email. please try again later" });
+    }
 });
 
 export default router;
