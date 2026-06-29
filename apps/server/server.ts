3a39da37b5bcc2497f7e4d108136b74c84dfe35e
Givi <gvtsvariani@gmail.com>
2026-06-29 17:41:38 -0400

    saving for a sec

--- apps/server/server.ts
+++ apps/server/server.ts
@@ -12,7 +12,7 @@ const app: express.Application = express.default();
 app.use(cors.default());
 app.use(express.json());
 
-app.use("/images", express.static(path.join(__dirname, "../db//data/img/")));
+app.use("/images", express.static(path.join(__dirname, "../db/data/img/")));
 
 app.use("/api", APIRoutes.default);
 
