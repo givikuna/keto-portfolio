26b2cfb17cbc80b856aaf1ac0515932e8954da7b
Givi <gvtsvariani@gmail.com>
2026-06-30 20:25:42 -0400

    sidebar

---
+++ apps/cms/src/components/Sidebar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Tree from "./Tree.svelte";
+</script>
+
+<div class="sidebar-panel">
+    <h2>Navigation</h2>
+    <Tree />
+</div>
+
+<style>
+    .sidebar-panel {
+        padding: 0.5rem;
+    }
+</style>
